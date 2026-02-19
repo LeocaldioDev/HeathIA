1e7ca7b2ac188cb21982a90b8afc310680a4a0c4
Leocaldio Carlos <leocaldiocarloss@gmail.com>
2026-02-19 17:18:54 +0100

    feat: add cors prod config

--- HealthIA.API/Program.cs
+++ HealthIA.API/Program.cs
@@ -30,7 +30,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("FrontendPolicy", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",           // Dev local
+                "https://heathia.onrender.com"     // Produção
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
